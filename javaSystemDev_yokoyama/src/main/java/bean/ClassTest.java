6e7fafa9a58776cd6610e4ebc69b3d726f7b892c
FragrantResult186 <FragrantResult186@gmail.com>
2026-04-23 10:03:14 +0900

    Beanクラスの追加

---
+++ javaSystemDev_yokoyama/src/main/java/bean/ClassTest.java
@@ -0,0 +1,19 @@
+package bean;
+
+public class ClassTest {
+	public String name;
+	public int point;
+	
+	public String getName() {
+		return name;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+}
---
+++ javaSystemDev_yokoyama/src/main/java/bean/Subject.java
@@ -0,0 +1,26 @@
+package bean;
+
+public class Subject {
+	public String schoolCD;
+	public String cd;
+	public String name;
+	
+	public String getSchoolCD() {
+		return schoolCD;
+	}
+	public String getCD() {
+		return cd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setSchoolCD(String schoolCD) {
+		this.schoolCD = schoolCD;
+	}
+	public void setCD(String cd) {
+		this.cd = cd;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
